90c7458ccee74514bbdbcd82ef149d86993761f0
Luciano Carricart <wzv135@haw-hamburg.de>
2026-04-26 22:03:35 +0200

    Guide to manually build .elf files using arm-none-eabi-gdb

--- README.md
+++ README.md
@@ -1,8 +1,39 @@
-stm32f4-nucleo-basic-template
+# Manual GCC Build, Flash, and Debug Workflow for STM32F401RE
+A command-line guide for manually building an ELF file, flashing an STM32 target, and debugging (without an IDE) using GNU ARM Embedded toolchain. The workflow creates toolchain awareness that transfers directly to Zephyr development.
 =============================
 
-Template to build projects for STM32 Nucleo F401RE board with GCC ARM Embedded toolchain
+## References
+This repository is a fork of metabr's template                              (https://github.com/metabr/stm32-nucleo-f401re-basic-template.git)
+The following video helps putting the pieces together                       (https://www.youtube.com/watch?v=-p26X8lTAvo)
 
-This repository is a demo project based on one provided by STMicroelectronics with STM32Cube firmware version 1.3.0.
+## Step 1: Toolchain
+- VSC and its extensions as shown in the video
+    - C/C++
+    - CMake
+    - Cortex Debug
+    - Makefile Tools
+- xPack OpenOCD and GNU ARM Embedded toolchain
+- MSYS2 and its packages (e.g., make, python3)
+    - Add the installed gcc-arm-none-eabi /bin folder to the MSYS2 path     (export PATH="$PATH:yourOwnPath")
 
-You may use it if you want to build programs to run on Nucleo F401RE boards with GNU ARM toolchain (https://launchpad.net/gcc-arm-embedded) and includes Eclipse project files.
+## Step 2: makefile
+- The libopencm3-examples repository used in the video is not useful to this guide, and is the reason why this repository exists. However, that repository contains a wide variety of examples spanning different MCU vendors and families, and is worth considering when looking for a template for a different device.
+- With MSYS2, sit on the root directory of this repository (the project to be debugged) and type the command "make". This will generate all relevant files to flash our MCU.
+    - .elf
+    - .bin
+    - .hex
+
+## Step 3: Debug via VSC extension
+- VSC Run and Debug > create a launch.json file > Cortex Debug > Add Configuration > Cortex Debug: OpenOCD > Delete the existing configuration
+- Change the path of the .elf
+- Modify the configFiles parameters
+    "configFiles": [
+    "interface/stlink.cfg",
+    "target/stm32f4x.cfg"
+    ]
+- Follow the Youtube video from minute 24:00 to set the required settings.json. These paths are relative to the installation of both arm-none-eabi-gdb and OpenOCD in your computer.
+
+## Step 4: Upload the binary (.elf)!
+Once all the steps are followed correctly, and the directory paths point to the right pc locations, we can press "Start Debugging (F5)" in VSC. This will upload the .elf and allow us granular control, debugging line by line and seeing our internal LED blink repeteadly.
+
+If you would like to modify the code and re-build, simply open MSYS2, sit on the project directory, and use the command "make clean all". This will build the new .elf file!
--- src/main.c
+++ src/main.c
@@ -1,37 +1,13 @@
 /**
   ******************************************************************************
-  * @file    Templates/Src/main.c 
+  * @file    Templates/Src/main.c
   * @author  MCD Application Team
   * @version V1.1.0
   * @date    26-June-2014
   * @brief   Main program body
   ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT(c) 2014 STMicroelectronics</center></h2>
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
+  * @brief
+  * Blink the internal LED with 100 ms period.
   ******************************************************************************
   */
 
@@ -44,80 +20,65 @@
 
 /** @addtogroup Templates
   * @{
-  */ 
-
-/* Variables used for USB */
-USBD_HandleTypeDef  hUSBDDevice;
+  */
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
-#define CURSOR_STEP     7
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
 static void SystemClock_Config(void);
 static void Error_Handler(void);
-static uint32_t Demo_USBConfig(void);
 
 /* Private functions ---------------------------------------------------------*/
 
 /**
-  * @brief  Main program
+  * @brief  Main program.
   * @param  None
   * @retval None
   */
 int main(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /* STM32F4xx HAL library initialization:
-       - Configure the Flash prefetch, Flash preread and Buffer caches
-       - Systick timer is configured by default as source of time base, but user 
-             can eventually implement his proper time base source (a general purpose 
-             timer for example or other time source), keeping in mind that Time base 
-             duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and 
-             handled in milliseconds basis.
-       - Low Level Initialization
-     */
+  /*
+   * STM32F4xx HAL library initialization:
+   * - Configure Flash prefetch, preread and buffer caches.
+   * - Configure SysTick timer as the default HAL time base.
+   * - Perform low-level initialization.
+   */
   HAL_Init();
-  
-  /* Configure the System clock to 84 MHz */
-  SystemClock_Config();
-  
-  /* Add your application code here
-     */
-  GPIO_InitTypeDef  GPIO_InitStruct;
 
+  /* Configure the system clock to 84 MHz. */
+  SystemClock_Config();
+
+  /* Enable GPIOA peripheral clock. */
   __GPIOA_CLK_ENABLE();
-  
-  /* -2- Configure PA05 IO in output push-pull mode to
-         drive external LED */
+
+  /*
+   * Configure PA5 as output push-pull.
+   * On the NUCLEO-F401RE, PA5 is connected to the user LED.
+   */
   GPIO_InitStruct.Pin = GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
 
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); 
-  
-  /* USB configuration */
-  Demo_USBConfig();
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  uint8_t buf[4] = {0, CURSOR_STEP, CURSOR_STEP, 0};
-
-  /* Infinite loop */
+  /* Infinite loop. */
   while (1)
-  {    
+  {
     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
 
-    /* Insert delay 100 ms */
-    HAL_Delay(100);
-
-    USBD_HID_SendReport (&hUSBDDevice, buf, 4);
+    /* Insert delay of 100 ms. */
+    HAL_Delay(100U);
   }
 }
 
 /**
-  * @brief  System Clock Configuration
-  *         The system Clock is configured as follow : 
+  * @brief  System Clock Configuration.
+  *         The system clock is configured as follows:
   *            System Clock source            = PLL (HSI)
   *            SYSCLK(Hz)                     = 84000000
   *            HCLK(Hz)                       = 84000000
@@ -131,25 +92,25 @@ int main(void)
   *            PLL_Q                          = 7
   *            VDD(V)                         = 3.3
   *            Main regulator output voltage  = Scale2 mode
-  *            Flash Latency(WS)              = 2 
+  *            Flash Latency(WS)              = 2
   * @param  None
   * @retval None
   */
 static void SystemClock_Config(void)
 {
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  HAL_StatusTypeDef status;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
 
-  /* Enable Power Control clock */
+  /* Enable Power Control clock. */
   __PWR_CLK_ENABLE();
-  
-  /* The voltage scaling allows optimizing the power consumption when the device is 
-     clocked below the maximum system frequency, to update the voltage scaling value 
-     regarding system frequency refer to product datasheet.  */
+
+  /*
+   * Voltage scaling allows optimizing power consumption when the device is
+   * clocked below the maximum system frequency.
+   */
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
-  /* Enable HSI Oscillator and activate PLL with HSI as source */
+  /* Enable HSI oscillator and activate PLL with HSI as source. */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = 0x10;
@@ -159,52 +120,29 @@ static void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLN = 336;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
   RCC_OscInitStruct.PLL.PLLQ = 7;
-  if((status = HAL_RCC_OscConfig(&RCC_OscInitStruct)) != HAL_OK)
+
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
   }
- 
-  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
-     clocks dividers */
-  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+
+  /*
+   * Select PLL as system clock source and configure HCLK, PCLK1 and PCLK2
+   * clock dividers.
+   */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK |
+                                 RCC_CLOCKTYPE_HCLK |
+                                 RCC_CLOCKTYPE_PCLK1 |
+                                 RCC_CLOCKTYPE_PCLK2);
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;  
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  
-  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }  
-  
-  /* Enable HSE Oscillator
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  
-  if((status = HAL_RCC_OscConfig(&RCC_OscInitStruct)) != HAL_OK)
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
-  */
-}
-
-/**
-  * @brief  Initializes the USB for the demonstration application.
-  * @param  None
-  * @retval None
-  */
-static uint32_t Demo_USBConfig(void)
-{
-  /* Init Device Library */
-  USBD_Init(&hUSBDDevice, &HID_Desc, 0);
-  
-  /* Add Supported Class */
-  USBD_RegisterClass(&hUSBDDevice, USBD_HID_CLASS);
-  
-  /* Start Device Process */
-  USBD_Start(&hUSBDDevice);
-  
-  return 0;
 }
 
 /**
@@ -214,31 +152,30 @@ static uint32_t Demo_USBConfig(void)
   */
 static void Error_Handler(void)
 {
-  /* User may add here some code to deal with this error */
-  while(1)
-  {
-  }
-}
-
-#ifdef  USE_FULL_ASSERT
-
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t* file, uint32_t line)
-{ 
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
-  /* Infinite loop */
   while (1)
   {
   }
 }
+
+#ifdef USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name.
+  * @param  line: assert_param error line source number.
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  (void)file;
+  (void)line;
+
+  while (1)
+  {
+  }
+}
+
 #endif
 
 /**
